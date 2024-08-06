d2f76f605beb7f77189d1bd3ae4c605de6daf2dc
Jose Pereda <joselpereda@hotmail.com>
2024-08-06 14:48:16 -0700

    v1 code

---
+++ 02_Day_Variables_builtin_functions/02_exercise1.py
@@ -0,0 +1,25 @@
+print('Day 2: 30 Days of python programming')
+firstName = 'Jose'
+lastName = 'Pereda'
+fullName = firstName + ' ' + lastName
+Country = 'USA'
+City = 'San Diego'
+Age = 49
+isMarried = True
+isTrue = True
+isLightOn = isTrue
+hasChildren, isMale, isTall = True, True, True
+
+print('firstName: ', type(firstName))
+print('lastName: ', type(lastName))
+print('fullName: ', type(fullName))
+print('Country: ', type(Country))
+print('Age: ', type(Age))
+print('isMarried: ', type(isMarried))
+print('isTrue: ', type(isTrue))
+print('hasChildren: ', type(hasChildren))
+print('isMale: ', type(isMale))
+print('isTall: ', type(isTall))
+
+print('Length of first name is ' + str(len(firstName)) + ' ,while length of last name is ' + str(len(lastName)))
+print("_" * 80)

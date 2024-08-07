970bcee31c31bd5387ccfd280cef24e7a5a7fc0a
Jose Pereda <joselpereda@hotmail.com>
2024-08-06 17:28:41 -0700

    completed exercises

--- 02_Day_Variables_builtin_functions/02_exercise1.py
+++ 02_Day_Variables_builtin_functions/02_exercise1.py
@@ -1,3 +1,5 @@
+import math 
+
 print('Day 2: 30 Days of python programming')
 firstName = 'Jose'
 lastName = 'Pereda'
@@ -10,6 +12,7 @@ isTrue = True
 isLightOn = isTrue
 hasChildren, isMale, isTall = True, True, True
 
+#Exercise #1
 print('firstName: ', type(firstName))
 print('lastName: ', type(lastName))
 print('fullName: ', type(fullName))
@@ -21,5 +24,39 @@ print('hasChildren: ', type(hasChildren))
 print('isMale: ', type(isMale))
 print('isTall: ', type(isTall))
 
+#Exercise #s 2 & 3
 print('Length of first name is ' + str(len(firstName)) + ' ,while length of last name is ' + str(len(lastName)))
 print("_" * 80)
+
+#Exercise #4
+#CALCULATE
+numOne, numTwo = 5, 4
+sum = numTwo + numOne
+Diff = numTwo - numOne
+Product = numOne * numTwo
+Divide = numOne / numTwo
+Mod = numTwo % numOne
+Power = numOne ^ numTwo
+floorDiv = numOne // numTwo
+
+#PRINT TO SCREEN
+print("First Number: ", numOne)
+print("Second Number: ", numTwo)
+print("SUM = ", str(sum))
+print("DIFF = ", str(Diff))
+print("PRODUCT = ", str(Product))
+print("Quotient = ", str(Divide))
+print("Modulus = ", str(Mod))
+print("Exponent = ", str(Power))
+print("Floor Div = ", str(floorDiv))
+print("_" * 80)
+
+#Exercise #5
+Radius = 30
+Radius= int(input("Enter Radius of circle: "))
+areaOfCircle = math.pi * Radius ** 2
+circOfCircle = 2 * math.pi * Radius
+
+print("Area of Circle: ", round(areaOfCircle,2), " meters sqr")
+print("Circumfrence of Circle: ", round(circOfCircle,2), " meters")
+
